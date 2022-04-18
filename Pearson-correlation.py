724336b5b6218c7b88b027e98fea5eefafa12870
yurkish <rassadinj@gmail.com>
2022-04-18 15:20:08 +0300

    august interesting correlation

--- Pearson-correlation.py
+++ Pearson-correlation.py
@@ -5,23 +5,30 @@ from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 from scipy import stats
 # importing data from csv files
-month_code = 'june'
+month_code = 'aug'
+
 match month_code:
     case 'june':
         data5 = import_csv_temperature('june/r5.csv')
         data6 = import_csv_temperature('june/r6.csv')
         data7 = import_csv_temperature('june/r7.csv')
         dataweather = import_csv_weather('june/w1.csv')
+        time_point_amount = 800
+        research_amount = 100
     case 'july':
         data5 = import_csv_temperature('july/room5_july.csv')
         data6 = import_csv_temperature('july/room6_july.csv')
         data7 = import_csv_temperature('july/room7_july.csv')
         dataweather = import_csv_weather('july/met.csv')
+        time_point_amount = 800
+        research_amount = 100
     case 'aug':
         data5 = import_csv_temperature('aug/r5_aug.csv')
         data6 = import_csv_temperature('aug/r6_aug.csv')
         data7 = import_csv_temperature('aug/r7_aug.csv')
         dataweather = import_csv_weather('aug/w_aug.csv')
+        time_point_amount = 8000
+        research_amount = 700
 
 # define time interval that is covered by data from all three rooms
 time_start = max(data5[1][1], data6[1][1], data7[1][1])
@@ -56,7 +63,7 @@ room5_inter = interp1d(data5x, data5y, kind='cubic')
 room6_inter = interp1d(data6x, data6y, kind='cubic')
 room7_inter = interp1d(data7x, data7y, kind='cubic')
 #weath_inter = interp1d(datawx, datawy, kind='cubic')
-x1new = np.linspace(time_start, time_stop, num=800, endpoint=True)
+x1new = np.linspace(time_start, time_stop, num=time_point_amount, endpoint=True)
 
 time_steps = len(x1new)
 print('time steps = ', time_steps)
@@ -107,7 +114,7 @@ y_s = np.array(y_simple)
 #plt.show()
 my_rho = np.corrcoef(x_s, y_s)
 
-research_amount = 100
+
 issleduem = np.zeros(research_amount)
 print('pearson coefficient = ',my_rho)
 
@@ -122,7 +129,9 @@ print(np.where(issleduem == max(issleduem)))
 print(np.argmax(issleduem))
 q = np.argmax(issleduem)
 #print(issleduem.index(issleduem.max()))
-
+f1 = plt.figure(1)
+plt.plot([1, 2, 3])
+plt.title("Figure 1 not cleared clf()")
 slope, intercept, r, p, std_err = stats.linregress(y_s[:h-q],x_s[q:])
 
 def myfunc(xxx):
