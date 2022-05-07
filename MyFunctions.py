e79aedce4075e5965b3ae1c49817db244e651c10
“Yurkish” <rassadinj@gmail.com>
2022-05-07 20:33:11 +0300

    subplots

--- MyFunctions.py
+++ MyFunctions.py
@@ -7,6 +7,7 @@ from scipy.interpolate import interp1d
 from tkinter import filedialog
 from tkinter import *
 from scipy.interpolate import CubicSpline
+from scipy import stats
 kind_lst = ['nearest', 'zero', 'slinear', 'cubic', 'previous', 'next']
 
 def import_csv_temperature(csvfilename):
@@ -99,7 +100,7 @@ def pearson_correlation_research(month_code,room):
                 case 'room7':
                     data = import_csv_temperature('dataset/aug20/ERS-CO2_aud._7.csv')
             time_start = 1597613321
-            time_stop = 1598591287
+            time_stop  = 1598591287
 
     x = []
     y = []
@@ -111,17 +112,22 @@ def pearson_correlation_research(month_code,room):
         else:
             x.append(float(row[1]))
             y.append(float(row[2]))
-    x_new = np.linspace(time_start+360, time_stop-360, num=time_point_amount, endpoint=True)
-
-    #
+    #########################################################################
+    ### new time points net ###########
+    time_new = np.linspace(x[0], x[-1], num=time_point_amount, endpoint=True)
+    #########################################################################
+    ### interpolated function of temperature inside the room ################
+    r_t = interp1d(x, y, kind='linear')
+    #########################################################################
     # here we get data from meteostation dataset
     # time of measurement matches with time point for interpolation
     #
     ind = 1
     indd = 0
     w = np.zeros(time_point_amount)
+    room_inter = np.zeros(time_point_amount)
     fin = int(dataweather[-1][0])
-    for row in x_new:
+    for row in time_new:
         time_is_now = float(row)
         for i in range(ind, fin):
             if int(dataweather[i][1]) < time_is_now:
@@ -130,21 +136,72 @@ def pearson_correlation_research(month_code,room):
                 ind = i
                 break
         w[indd] = "%.7f" % float(dataweather[ind][2])
+        room_inter[indd] = "%.7f" % float(r_t(time_is_now))
         indd += 1
-    cs = CubicSpline(x, y)
+    #cs = CubicSpline(x, y)
 
     # for k in kind_lst:
     #     f = interp1d(x, y, kind=k)
     #     y_new = f(x_new)
     #     plt.plot(x_new, y_new, label=k)
-    room_funct = interp1d(x, y, kind='linear')
-    R = len(x)
-    print('\n R = ',R, '\n')
-    for row in range(R):
-        diff1 = cs(x[row]) - y[row]
-        diff2 = y[row] - room_funct(x[row])
-        print('d[2] = ', data[row][2], 'y=',y[row] ,' cs=', cs(data[row][1]),' diff1 = ', diff1 ,' diff2 = ', diff2 ,'\n')
-    plt.plot(x,y,'*',x, room_funct(x),'-',x_new, cs(x_new),'-.')
-    plt.legend(['raw','linear','cubic'], loc='best')
+
+    # R = len(x)
+    # print('\n R = ',R, '\n')
+    # for row in range(R):
+    #     diff1 = cs(x[row]) - y[row]
+    #     diff2 = y[row] - room_funct(x[row])
+    #     print('d[2] = ', data[row][2], 'y=',y[row] ,' cs=', cs(data[row][1]),' diff1 = ', diff1 ,' diff2 = ', diff2 ,'\n')
+    #########################################################
+    ##### here we start our estimation research #############
+    #########################################################
+    room_weather_corr = np.corrcoef(room_inter, w)
+    print(room_weather_corr)
+    #########################################################
+    ##### let's shift reading frame #########################
+    #########################################################
+    search_for_maximum_corr = np.zeros(research_amount)
+    #
+    w_reduced = w[:time_point_amount - research_amount]
+    for i in range(research_amount):
+        # room temperature with i-shifted frame
+        room_research = room_inter[i:time_point_amount - research_amount + i]
+        # correlation of shifted room-t frame and fixed weather-t frame
+        search_for_maximum_corr[i] = np.corrcoef(room_research, w_reduced)[0][1]
+        print('pearson coefficient ', i, ' = ', search_for_maximum_corr [i])
+    print(np.where(search_for_maximum_corr  == max(search_for_maximum_corr)))
+    print(np.argmax(search_for_maximum_corr ))
+    ########################################################
+    ### best for correlation shift size ####################
+    chosen_shift = np.argmax(search_for_maximum_corr)
+    ###################################################
+    slope, intercept, r, p, std_err = stats.linregress(w_reduced, room_inter[chosen_shift:time_point_amount - research_amount + chosen_shift])
+
+    def linear_room_weather_prediction(x):
+        return slope * x + intercept
+    #########################################################################
+    ###### prepareing
+    mymodel = list(map(linear_room_weather_prediction, w[:time_point_amount]))
+
+    figure_shifting, (initial_fig, correl_to_shift, shifted_fig) = plt.subplots(3, 1, constrained_layout=True, sharey=True)
+    initial_fig.plot(time_new,r_t,'-')
+    initial_fig.plot(x_new[research_amount:], x[research_amount:], '-.')
+    initial_fig.set_title('damped')
+    initial_fig.set_xlabel('time (s)')
+    initial_fig.set_ylabel('amplitude')
+
+    correl_to_shift.plot(x_new[research_amount:], myfunc5(y_s[q:time_point_amount - research_amount + q]))
+    correl_to_shift.set_xlabel('time (s)')
+    correl_to_shift.set_title('undamped')
+
+    shifted_fig.plot(x_res[q:time_point_amount - research_amount + q], my(y_s[q:time_point_amount - research_amount + q]))
+    shifted_fig.set_xlabel('time (s)')
+    shifted_fig.set_title('undamped')
+
+    fig.suptitle('Different types of oscillations', fontsize=16)
+    plt.show()
+    plt.plot()
+
+    plt.plot(x,y,'*',x_new, r_t(x_new),'-')
+    plt.legend(['raw','linear'], loc='best')
     plt.show()
     return 1
